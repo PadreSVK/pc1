2ccefcabf54631945e002795e7f430bcc87bdb0b
Patrik Švikruha <svikruha@netsearch.cz>
2019-12-04 14:15:41 +0100

    add sample 10

---
+++ Cvicenie_10/AutaDB.h
@@ -0,0 +1,17 @@
+
+#ifndef __AUTADB_H__
+#define __AUTADB_H__
+
+#define ZNACKA_SIZE 21
+
+struct t_auto
+{
+    char  znacka[ZNACKA_SIZE];
+    int   rok;
+    struct t_auto* dalsi;
+};
+
+void add( char * znacka, int rok, struct t_auto ** uk_prvni );
+void del( int rok, struct t_auto ** uk_prvni );
+
+#endif
